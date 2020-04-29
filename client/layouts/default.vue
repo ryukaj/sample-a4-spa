ecef42610c82e32640ffb696e326ddd5016c3288
ryukaj <ryukaj@gmail.com>
2020-04-29 09:16:59 +0900

    モック作成中

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -2,17 +2,23 @@
   v-app
     v-navigation-drawer(app v-model='drawer')
       v-list
-        v-list-item
+        v-list-item(to='/' nuxt)
           v-list-item-action
             v-icon mdi-home
           v-list-item-content
             v-list-item-title Home
+        v-list-item(to='/inspire' nuxt)
+          v-list-item-action
+            v-icon mdi-charity 
+          v-list-item-content
+            v-list-item-title sample
     v-app-bar(app)
       v-app-bar-nav-icon(@click.stop="drawer = !drawer")
       v-toolbar-title A4 Header
     v-content
       v-container
-        nuxt
+        transition(name='page')
+          nuxt
     v-footer(app)
       span A4 Footer
 </template>
