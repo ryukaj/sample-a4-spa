dd8e2d47b7f09f553354c27c341079e3711c8686
ryukaj <ryukaj@gmail.com>
2020-05-09 22:29:32 +0900

    parallax fixed

--- client/pages/parallax.vue
+++ client/pages/parallax.vue
@@ -1,11 +1,20 @@
 <template lang="pug">
   main
+    v-navigation-drawer(app v-model="drawerRight" clipped right)
+      v-list(dense)
+        v-list-item
+          v-list-item-content
+            v-list-item-title Log In
+        v-list-item
+          v-list-item-content
+            v-list-item-title New Entry
+        v-list-item
+          v-list-item-content
+            v-list-item-title Try
     v-app-bar(app v-if='parallax.subHeight < position')
       v-toolbar-title forAdvice
       v-spacer
-      div.dummylink Log In
-      div.dummylink New Entry
-      div.dummylink Try
+      v-app-bar-nav-icon(@click.stop="drawerRight = !drawerRight")
     v-parallax.l-parallax(:height='parallax.mainHeight' src='images/parallax/main.png')
       .l-parallax__content.l-parallax__msg
         div エンジニアのことはエンジニアに聞け
@@ -14,13 +23,13 @@
         v-row
           v-col(cols=12 align='center') forAdvice
         v-row.l-parallax__nav__links(align='center' justify='center')
-          v-col(cols=3 align='center') Log In
-          v-col(cols=3 align='center') New Entry
-          v-col(cols=3 align='center') Try
+          v-col(cols=4 md=3 align='center') Log In
+          v-col(cols=4 md=3 align='center') New Entry
+          v-col(cols=4 md=3 align='center') Try
         v-row(align='center' justify='center')
-          v-col(cols=3 align='center') 会員の方はこちら
-          v-col(cols=3 align='center') 新規登録はこちら
-          v-col(cols=3 align='center') 登録せずに見たい方はこちら
+          v-col(cols=4 md=3 align='center') 会員の方はこちら
+          v-col(cols=4 md=3 align='center') 新規登録はこちら
+          v-col(cols=4 md=3 align='center') 登録せずに見たい方はこちら
     v-parallax.l-parallax(:height='parallax.subHeight' src='images/parallax/sub1.png' style='margin-top:100px;')
 </template>
 
@@ -28,6 +37,7 @@
 export default {
   layout: 'guest',
   data: () => ({
+    drawerRight: null,
     parallax: {
       mainHeight: 600,
       subHeight: 500
@@ -57,17 +67,19 @@ export default {
   background-color: #000;
 }
 .l-parallax__content {
-  width: 960px;
+  width: 100%;
+  max-width: 960px;
   margin: auto;
 }
 .l-parallax__msg {
   font-size: xx-large;
 }
 .l-parallax__nav {
-  color: #1631a3;
+  color: aqua;
+  font-weight: bold;
 }
 .l-parallax__nav__links {
-  border-top: 2px solid #1631a3;
-  border-bottom: 2px solid #1631a3;
+  border-top: 2px solid aqua;
+  border-bottom: 2px solid aqua;
 }
 </style>
