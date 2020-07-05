3419bb0cd531c41fd4c92b99ad659783b2fbb127
ryukaj <ryukaj@gmail.com>
2020-07-05 21:29:16 +0900

    mypage created

---
+++ client/layouts/prototype.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  v-app
+    v-navigation-drawer(app v-model='drawer')
+      v-list
+        v-list-item
+          v-avatar
+            v-img(src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg")
+          v-list-item-title 〇〇〇〇さん
+        v-list-item(to='/' nuxt)
+          v-list-item-action
+            v-icon mdi-home
+          v-list-item-content
+            v-list-item-title アドバイスする
+        v-list-item(to='/' nuxt)
+          v-list-item-action
+            v-icon mdi-home
+          v-list-item-content
+            v-list-item-title キャリア登録・更新
+        v-list-item(to='/' nuxt)
+          v-list-item-action
+            v-icon mdi-home
+          v-list-item-content
+            v-list-item-title 受取一覧
+        v-list-item(to='/' nuxt)
+          v-list-item-action
+            v-icon mdi-home
+          v-list-item-content
+            v-list-item-title 実施一覧
+        v-list-item(to='/' nuxt)
+          v-list-item-action
+            v-icon mdi-home
+          v-list-item-content
+            v-list-item-title 登録情報更新
+    v-app-bar(app)
+      v-app-bar-nav-icon(@click.stop="drawer = !drawer")
+      v-toolbar-title A4 Mypage
+    v-content
+      v-container
+        transition(name='page')
+          nuxt
+    v-footer(app)
+      span A4 Footer
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  }),
+  created: () => {}
+}
+</script>
---
+++ client/pages/mypage/index.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+main.l-main
+  v-container
+    v-row
+      v-col(cols='12' sm='2')
+        v-row
+          v-col アドバイス受取数
+            div ●●件
+        v-row
+          v-col アドバイス実施数
+            div ●●件
+        v-row
+          v-col ●●さんと同じ職種登録数
+            div ●●人
+        v-row
+          v-col エンジニア総登録数
+            div ●●人
+      v-col(cols='12' sm='6')
+        v-row
+          v-col(cols='12' sm='6') スペシャリストランク
+            div S6
+          v-col(cols='12' sm='6') マネジメントランク
+            div M4
+        v-row
+          v-col(cols='12') アドバイス実施エンジニアからの返信
+            div aaaaaa
+            div bbbbbb
+            div ccccc
+        v-row
+          v-col(cols='12') 事務局からのご連絡
+            div aaaaaa
+            div bbbbbb
+            div ccccc
+        v-row
+          v-col(cols='12') レーダーチャート配置
+      v-col(cols='12' sm='4')
+        v-row
+          v-col(cols='12') これまでの経験
+            v-timeline
+              v-timeline-item
+                v-card sample1
+              v-timeline-item
+                v-card sample2
+              v-timeline-item
+                v-card sample3
+        v-row
+          v-col(cols='12') これからのキャリア
+           v-timeline
+            v-timeline-item
+              v-card sample1
+            v-timeline-item
+              v-card sample2
+            v-timeline-item
+              v-card sample3
+
+</template>
+
+<script>
+import { Radar } from 'vue-chartjs'
+
+export default {
+  extends: Radar,
+  layout: 'prototype',
+  created: () => {},
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.l-main {
+    position: relative;
+}
+</style>
